49b47d25f16bc28dd179a5b3948d361bfcfbffcf
Tomas Pesek <pesekt@gmail.com>
2024-06-13 15:02:14 +0200

    -

--- todo-api/index.ts
+++ todo-api/index.ts
@@ -12,9 +12,9 @@ app.use(express.json());
 app.use("/todos", todoRouter);
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World!!!!");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
 });
